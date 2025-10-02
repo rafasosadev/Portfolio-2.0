05e58c3e9fee0714e504b3e598940f4d8130f95f
Rafa Sosa <rafasl1993@gmail.com>
2025-10-02 19:16:09 +0200

    Cambiar proyectos

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,6 @@ import Particle from "../Particle";
 import calculadora from "../../Assets/Projects/calculadora.PNG";
 
 import SosaCars from "../../Assets/Projects/SosaCars.png";
-import chatify from "../../Assets/Projects/chatify.png";
 
 import celiacosmy from "../../Assets/Projects/celiacosmy.png";
 
@@ -21,18 +20,6 @@ function Projects() {
           Aquí algunos de los proyectos en los que he trabajado recientemente.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={celiacosmy}
@@ -68,32 +55,6 @@ function Projects() {
               languages={['html5','css3', 'javascript', 'vercel']}
             />
           </Col>
-
-{/* PONER MÁS PROYECTOS AQUI ABAJO EJEMPLO:  
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
-*/}
-
         </Row>
       </Container>
     </Container>
